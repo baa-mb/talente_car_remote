29b4b8e6b40a994ee5e3397a51f34f9e2b4d4256
baa <34665120+baa-mb@users.noreply.github.com>
2026-07-02 15:16:26 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,53 @@
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendValue("gerade", 0)
+    radio.sendValue("kurve", 0)
+})
+function neigungen () {
+    gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
+    gerade = Math.round(gerade / g_empfind) * g_empfind
+    kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
+}
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("get_dist", 1)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "distanz") {
+        basic.showNumber(value)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+let kurve = 0
+let gerade = 0
+let g_empfind = 0
+radio.setGroup(99)
+basic.showString("99")
+g_empfind = 5
+let ist_oben = true
+let alt_gerade = -99
+let alt_kurve = -99
+let k_empfind = 1
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    neigungen()
+    if (input.buttonIsPressed(Button.A)) {
+        if (gerade != alt_gerade || kurve != alt_kurve) {
+            radio.sendValue("gerade", gerade)
+            radio.sendValue("kurve", kurve)
+        }
+    }
+    alt_gerade = gerade
+    alt_kurve = kurve
+})
