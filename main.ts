b6e26eee3c71c10a018bda43128f122fd8182f1d
baa <34665120+baa-mb@users.noreply.github.com>
2026-07-10 18:58:46 +0200

    Add sende_stopp function and modify button A behavior

--- main.ts
+++ main.ts
@@ -1,14 +1,32 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    sende_stopp()
+})
+function sende_stopp () {
     radio.sendValue("gerade", 0)
     radio.sendValue("kurve", 0)
-})
+}
 function neigungen () {
     gerade = Math.min(Math.max(input.rotation(Rotation.Pitch), -45), 45)
     gerade = Math.round(gerade / g_empfind) * g_empfind
     kurve = Math.min(Math.max(input.rotation(Rotation.Roll), -45), 45)
     kurve = kurve / 5
 }
+input.onButtonPressed(Button.AB, function () {
+    beweg_flag = !(beweg_flag)
+    if (beweg_flag) {
+        basic.showString("A")
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
 input.onButtonPressed(Button.B, function () {
+    beweg_flag = false
     radio.sendValue("get_dist", 1)
 })
 radio.onReceivedValue(function (name, value) {
@@ -27,6 +45,8 @@ radio.onReceivedValue(function (name, value) {
 let kurve = 0
 let gerade = 0
 let g_empfind = 0
+let beweg_flag = false
+beweg_flag = false
 radio.setGroup(99)
 basic.showString("99")
 g_empfind = 1
@@ -43,7 +63,7 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     neigungen()
-    if (input.buttonIsPressed(Button.A)) {
+    if (input.buttonIsPressed(Button.A) || beweg_flag) {
         if (gerade != alt_gerade || kurve != alt_kurve) {
             radio.sendValue("gerade", gerade)
             radio.sendValue("kurve", kurve)
